14fd5f002dd13e75504fa635d48941aed5951cde
rspeicher <rspeicher@gmail.com>
2011-02-28 22:51:10 -0500

    Update spec to use our new TemporaryFiles module
    Also adds pending specs for our next features and restructures things a bit.

--- spec/wriggle_spec.rb
+++ spec/wriggle_spec.rb
@@ -1,73 +1,96 @@
 require 'spec_helper'
 
 describe Wriggle do
-  # 1.8.7's Find uses relative paths, while 1.9.2's uses absolutes,
-  # so we'll call expand_path and make sure our specs use this method
-  def valid_dir
-    File.expand_path(File.dirname(__FILE__))
-  end
-
   it "should require a path argument" do
-    lambda { wriggle() {} }.should raise_error(ArgumentError)
+    expect { wriggle() {} }.to raise_error(ArgumentError)
   end
 
   it "should raise an ArgumentError when given an invalid path" do
-    lambda { wriggle('/path/to/nothing') {} }.should raise_error(ArgumentError, /does not exist/)
+    expect { wriggle('/path/to/nothing') {} }.to raise_error(ArgumentError, /does not exist/)
   end
 
   it "should raise an ArgumentError when given a non-directory" do
-    lambda { wriggle(__FILE__) {} }.should raise_error(ArgumentError, /is not a directory/)
+    expect { wriggle(__FILE__) {} }.to raise_error(ArgumentError, /is not a directory/)
   end
 
-  context "given a valid path" do
-    describe "#file" do
-      it "should raise an ArgumentError when not given a block" do
-        lambda { wriggle(valid_dir) { |w| w.file } }.should raise_error(ArgumentError, /a block is required/)
-      end
+  it "should alias 'files' to 'file'" do
+    pending
+    expect { wriggle(TemporaryFiles.path) { |w| w.files } }.to_not raise_error(NoMethodError)
+  end
 
-      it "should not include directories when only a file block is provided" do
-        actual = []
-        wriggle(valid_dir) do |w|
-          w.file do |path|
-            actual << path
-          end
-        end
+  it "should alias 'directories' to 'directory'" do
+    pending
+    expect { wriggle(TemporaryFiles.path) { |w| w.directories } }.to_not raise_error(NoMethodError)
+  end
+end
 
-        actual.should include("#{valid_dir}/empty.txt")
-        actual.should include("#{valid_dir}/wriggle_spec.rb")
-        actual.should_not include("#{valid_dir}/spec")
-      end
+describe Wriggle, "#file" do
+  it "should raise an ArgumentError when not given a block" do
+    expect { wriggle(TemporaryFiles.path) { |w| w.file } }.to raise_error(ArgumentError, /a block is required/)
+  end
 
-      it "should only include files of the type specified" do
-        actual = []
-        wriggle(valid_dir) do |w|
-          w.file :rb do |path|
-            actual << path
-          end
-        end
-
-        actual.should include("#{valid_dir}/wriggle_spec.rb")
-        actual.should_not include("#{valid_dir}/empty.txt")
-        actual.should_not include("#{valid_dir}")
+  it "should not include directories when only a file block is provided" do
+    actual = []
+    wriggle(TemporaryFiles.path) do |w|
+      w.file do |path|
+        actual << path
       end
     end
 
-    describe "#directory" do
-      it "should raise an ArgumentError when not given a block" do
-        lambda { wriggle(valid_dir) { |w| w.directory } }.should raise_error(ArgumentError, /a block is required/)
+    actual.should include("#{TemporaryFiles.path}/app/controllers/members_controller.rb")
+    actual.should include("#{TemporaryFiles.path}/app/models/item.rb")
+    actual.should include("#{TemporaryFiles.path}/app/views/layouts/application.lua.erb")
+    actual.should include("#{TemporaryFiles.path}/app/views/layouts/application.html.haml")
+    actual.should include("#{TemporaryFiles.path}/app/views/members/wishlists/create.js.rjs")
+    actual.should_not include("#{TemporaryFiles.path}/app/views/")
+  end
+
+  it "should only include files of the type specified" do
+    ruby, view = [], []
+    wriggle(TemporaryFiles.path) do |w|
+      w.file :rb, :erb do |path|
+        ruby << path
       end
 
-      it "should not include files when only a directory block is provided" do
-        actual = []
-        wriggle(valid_dir) do |w|
-          w.directory do |path|
-            actual << path
-          end
-        end
-
-        actual.should include("#{valid_dir}")
-        actual.should_not include("#{valid_dir}/wriggle_spec.rb")
+      w.file %w(rjs haml) do |path|
+        view << path
       end
     end
+
+    ruby.should include("#{TemporaryFiles.path}/app/controllers/members_controller.rb")
+    ruby.should include("#{TemporaryFiles.path}/app/models/item.rb")
+    ruby.should include("#{TemporaryFiles.path}/app/views/layouts/application.lua.erb")
+    ruby.should_not include("#{TemporaryFiles.path}/app/views/layouts/application.html.haml")
+    ruby.should_not include("#{TemporaryFiles.path}/app/views/members/wishlists/create.js.rjs")
+    ruby.should_not include("#{TemporaryFiles.path}/app/views/")
+
+    view.should_not include("#{TemporaryFiles.path}/app/controllers/members_controller.rb")
+    view.should_not include("#{TemporaryFiles.path}/app/models/item.rb")
+    view.should_not include("#{TemporaryFiles.path}/app/views/layouts/application.lua.erb")
+    view.should include("#{TemporaryFiles.path}/app/views/layouts/application.html.haml")
+    view.should include("#{TemporaryFiles.path}/app/views/members/wishlists/create.js.rjs")
+    view.should_not include("#{TemporaryFiles.path}/app/views/")
   end
+end
+
+describe Wriggle, "#directory" do
+  it "should raise an ArgumentError when not given a block" do
+    expect { wriggle(TemporaryFiles.path) { |w| w.directory } }.to raise_error(ArgumentError, /a block is required/)
+  end
+
+  it "should not include files when only a directory block is provided" do
+    actual = []
+    wriggle(TemporaryFiles.path) do |w|
+      w.directory do |path|
+        actual << path
+      end
+    end
+
+    actual.should include("#{TemporaryFiles.path}")
+    actual.should include("#{TemporaryFiles.path}/app")
+    actual.should include("#{TemporaryFiles.path}/app/models")
+    actual.should_not include("#{TemporaryFiles.path}/models/member.rb")
+  end
+
+  it "should only include directories matching a pattern when given an argument"
 end
