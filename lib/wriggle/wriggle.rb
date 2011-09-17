7c7416d6f31e569b0bc3212f1f13d97ce578abe2
Robert Speicher <rspeicher@gmail.com>
2011-09-17 01:04:40 -0400

    Restructure so that the Wriggle class is in its own file

---
+++ lib/wriggle/wriggle.rb
@@ -0,0 +1,124 @@
+module Wriggle
+  class Wriggle
+    def initialize(root, &block)
+      @root             = root
+      @file_blocks      = []
+      @directory_blocks = []
+      @extension_blocks = []
+
+      crawl(&block)
+    end
+
+    # Define a block to be called when a file is encountered
+    #
+    # @example All files
+    #   file { |file| ... }
+    #
+    # @yield [path] Full, absolute file path
+    # @raise ArgumentError When no block provided
+    def file(*names, &block)
+      raise ArgumentError, "a block is required" unless block_given?
+      @file_blocks << {:names => names.flatten, :block => block}
+    end
+
+    # Define a block to be called when a directory is encountered
+    #
+    # Provide one or more patterns to match the directory's basename against
+    #
+    # @example All directories
+    #   directory { |dir| ... }
+    # @example Directories matching <tt>/lib/</tt>
+    #   directory(/lib/) { |dir| ... }
+    # @example Directories matching <tt>/lib/</tt> or <tt>/spec/</tt>
+    #   directories(/lib/, /spec/) { |dir| ... }
+    #
+    # @param [Array] patterns Limit the yielded directory paths only to those matching the provided pattern(s)
+    # @yield [path] Full, absolute directory path
+    # @raise ArgumentError When no block provided
+    def directory(*patterns, &block)
+      raise ArgumentError, "a block is required" unless block_given?
+      @directory_blocks << {:pattern => patterns.flatten, :block => block}
+    end
+
+    # Define a block to be called when a file of a certain extension is encountered
+    #
+    # @example All <tt>.rb</tt> files
+    #   extension('.rb') { |file| ... }
+    # @example All <tt>.rb</tt> or <tt>.erb</tt> files
+    #   extensions(:rb, :erb) { |file| ... }
+    # @example All video files
+    #   extensions(%w(mpeg mpeg wmv avi mkv)) { |file| ... }
+    #
+    # @param [Array] extensions Limit the yielded file paths to the provided extensions
+    # @yield [path] Full, absolute file path
+    # @raise ArgumentError When no block provided
+    # @raise ArgumentError When no extension argument provided
+    def extension(*extensions, &block)
+      raise ArgumentError, "a block is required" unless block_given?
+      raise ArgumentError, "at least one extension is required" unless extensions.length > 0
+      @extension_blocks << {:ext => extensions.flatten, :block => block}
+    end
+
+    alias_method :files,       :file
+    alias_method :directories, :directory
+    alias_method :extensions,  :extension
+
+    private
+
+    # Yields <tt>self</tt> to allow the user to define file/directory blocks, and then
+    # crawl the root directory, dispatching the user's defined blocks as each
+    # file type is encountered
+    def crawl(&block)
+      yield self
+
+      Find.find(@root) do |current|
+        if File.file?(current)
+          dispatch_file(current)
+        elsif File.directory?(current)
+          dispatch_directory(current)
+        end
+      end
+    end
+
+    # Called whenever <tt>crawl</tt> encounters a file
+    #
+    # Handles both <tt>file_blocks</tt> and <tt>extension_blocks</tt>.
+    def dispatch_file(path)
+      extension = File.extname(path)
+
+      @file_blocks.each do |group|
+        if group[:names].empty?
+          group[:block].call(path)
+        else
+          filename = File.basename(path)
+          if group[:names].any? { |v| (v.is_a?(String) and filename == v) or (v.is_a?(Regexp) and filename =~ v) }
+            group[:block].call(path)
+          end
+        end
+      end
+
+      @extension_blocks.each do |group|
+        # Check if any of the extensions match the current file's extension (with or without the period)
+        if group[:ext].any? { |v| v.to_s == extension or v.to_s == extension[1..-1] }
+          group[:block].call(path)
+        end
+      end
+    end
+
+    # Called whenever <tt>crawl</tt> encounters a directory
+    def dispatch_directory(path)
+      @directory_blocks.each do |group|
+        if group[:pattern].empty?
+          group[:block].call(path)
+        else
+          # User requested only directories matching a certain pattern
+          # Check if any of the patterns match the directory name
+          base = File.basename(path)
+          if group[:pattern].any? { |v| base =~ v }
+            group[:block].call(path)
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/wriggle/wriggle_spec.rb
@@ -0,0 +1,154 @@
+require 'spec_helper'
+
+module Wriggle
+  describe Wriggle do
+    it "should require a path argument" do
+      expect { wriggle() {} }.to raise_error(ArgumentError)
+    end
+
+    it "should raise an ArgumentError when given an invalid path" do
+      expect { wriggle('/path/to/nothing') {} }.to raise_error(ArgumentError, /does not exist/)
+    end
+
+    it "should raise an ArgumentError when given a non-directory" do
+      expect { wriggle(__FILE__) {} }.to raise_error(ArgumentError, /is not a directory/)
+    end
+
+    it "should alias 'files' to 'file'" do
+      expect { wriggle(TemporaryFiles.path) { |w| w.files } }.to_not raise_error(NoMethodError)
+    end
+
+    it "should alias 'directories' to 'directory'" do
+      expect { wriggle(TemporaryFiles.path) { |w| w.directories } }.to_not raise_error(NoMethodError)
+    end
+
+    it "should alias 'extensions' to 'extension'" do
+      expect { wriggle(TemporaryFiles.path) { |w| w.extensions } }.to_not raise_error(NoMethodError)
+    end
+  end
+
+  describe Wriggle, "#file" do
+    it "should raise an ArgumentError when not given a block" do
+      expect { wriggle(TemporaryFiles.path) { |w| w.file } }.to raise_error(ArgumentError, /a block is required/)
+    end
+
+    it "should only include files matching a pattern when given a regex argument" do
+      actual = []
+      wriggle(TemporaryFiles.path) do |w|
+        w.file /controller/ do |path|
+          actual << path
+        end
+      end
+
+      actual.should include("#{TemporaryFiles.path}/app/controllers/members_controller.rb")
+      actual.should include("#{TemporaryFiles.path}/app/controllers/members/wishlists_controller.rb")
+      actual.should_not include("#{TemporaryFiles.path}/app/controllers")
+      actual.should_not include("#{TemporaryFiles.path}/app/helpers/items_helper.rb")
+    end
+
+    it "should only include files matching a string when given a string argument" do
+      actual = []
+      wriggle(TemporaryFiles.path) do |w|
+        w.file 'members_controller.rb', 'wishlists_controller.rb' do |path|
+          actual << path
+        end
+      end
+
+      actual.should include("#{TemporaryFiles.path}/app/controllers/members_controller.rb")
+      actual.should include("#{TemporaryFiles.path}/app/controllers/members/wishlists_controller.rb")
+      actual.should include("#{TemporaryFiles.path}/app/controllers/wishlists_controller.rb")
+      actual.should_not include("#{TemporaryFiles.path}/app/controllers")
+      actual.should_not include("#{TemporaryFiles.path}/app/helpers/items_helper.rb")
+    end
+
+    it "should not include directories when only a file block is provided" do
+      actual = []
+      wriggle(TemporaryFiles.path) do |w|
+        w.file do |path|
+          actual << path
+        end
+      end
+
+      actual.should include("#{TemporaryFiles.path}/app/controllers/members_controller.rb")
+      actual.should include("#{TemporaryFiles.path}/app/models/item.rb")
+      actual.should include("#{TemporaryFiles.path}/app/views/layouts/application.lua.erb")
+      actual.should include("#{TemporaryFiles.path}/app/views/layouts/application.html.haml")
+      actual.should include("#{TemporaryFiles.path}/app/views/members/wishlists/create.js.rjs")
+      actual.should_not include("#{TemporaryFiles.path}/app/views/")
+    end
+  end
+
+  describe Wriggle, "#directory" do
+    it "should raise an ArgumentError when not given a block" do
+      expect { wriggle(TemporaryFiles.path) { |w| w.directory } }.to raise_error(ArgumentError, /a block is required/)
+    end
+
+    it "should not include files when only a directory block is provided" do
+      actual = []
+      wriggle(TemporaryFiles.path) do |w|
+        w.directory do |path|
+          actual << path
+        end
+      end
+
+      actual.should include("#{TemporaryFiles.path}")
+      actual.should include("#{TemporaryFiles.path}/app")
+      actual.should include("#{TemporaryFiles.path}/app/models")
+      actual.should_not include("#{TemporaryFiles.path}/models/member.rb")
+    end
+
+    it "should only include directories matching a pattern when given an argument" do
+      actual = []
+      wriggle(TemporaryFiles.path) do |w|
+        w.directory /member/, /model/ do |path|
+          actual << path
+        end
+      end
+
+      actual.should include("#{TemporaryFiles.path}/app/controllers/members")
+      actual.should include("#{TemporaryFiles.path}/app/helpers/members")
+      actual.should include("#{TemporaryFiles.path}/app/views/members")
+      actual.should_not include("#{TemporaryFiles.path}/app/views/members/wishlists")
+      actual.should include("#{TemporaryFiles.path}/app/models")
+      actual.should_not include("#{TemporaryFiles.path}/app/views/items")
+      actual.should_not include("#{TemporaryFiles.path}/app/views/layouts")
+    end
+  end
+
+  describe Wriggle, "#extension" do
+    it "should raise an ArgumentError when not given a block" do
+      expect { wriggle(TemporaryFiles.path) { |w| w.extension(:rb) } }.to raise_error(ArgumentError, /a block is required/)
+    end
+
+    it "should raise an ArgumentError when not given any extensions to match" do
+      expect { wriggle(TemporaryFiles.path) { |w| w.extension {} } }.to raise_error(ArgumentError, /at least one extension is required/)
+    end
+
+    it "should only include files of the extension specified" do
+      ruby, view = [], []
+      wriggle(TemporaryFiles.path) do |w|
+        w.extension :rb, :erb do |path|
+          ruby << path
+        end
+
+        w.extension %w(rjs haml) do |path|
+          view << path
+        end
+      end
+
+      ruby.should include("#{TemporaryFiles.path}/app/controllers/members_controller.rb")
+      ruby.should include("#{TemporaryFiles.path}/app/models/item.rb")
+      ruby.should include("#{TemporaryFiles.path}/app/views/layouts/application.lua.erb")
+      ruby.should_not include("#{TemporaryFiles.path}/app/views/layouts/application.html.haml")
+      ruby.should_not include("#{TemporaryFiles.path}/app/views/members/wishlists/create.js.rjs")
+      ruby.should_not include("#{TemporaryFiles.path}/app/views/")
+
+      view.should_not include("#{TemporaryFiles.path}/app/controllers/members_controller.rb")
+      view.should_not include("#{TemporaryFiles.path}/app/models/item.rb")
+      view.should_not include("#{TemporaryFiles.path}/app/views/layouts/application.lua.erb")
+      view.should include("#{TemporaryFiles.path}/app/views/layouts/application.html.haml")
+      view.should include("#{TemporaryFiles.path}/app/views/members/wishlists/create.js.rjs")
+      view.should_not include("#{TemporaryFiles.path}/app/views/")
+    end
+  end
+end
